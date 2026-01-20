008ef0ff849fed8da170583f6a055342d48311d4
BillyHJHamilton <BillyHJHamilton@gmail.com>
2026-01-20 08:16:33 -0500

    Remove Config file

--- Config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Types.h"
-
-void config_terminal();
-
--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,5 @@
 #include "BearLibTerminal.h"
 
-#include "Config.h"
 #include "Geometry.h"
 #include "Game.h"
 #include "Global.h"
@@ -25,6 +24,25 @@ void test_line_drawing()
 			terminal_put(itr->x, itr->y, 'a'+y/2);
 		}
 	}
+}
+
+void config_terminal()
+{
+	char const* FONT1 = "FSEX302.ttf";
+	char const* FONT2 = FONT1;
+
+	terminal_set("window.size=120x31");
+	terminal_set("window.title='TROLL'");
+	terminal_setf("font: %s, size=8x16", FONT1);
+	terminal_setf("tile font: %s, size=16x16, spacing=2x1", FONT2);
+
+	// register for input
+	terminal_set
+	(
+		"input.filter=[keyboard+]"
+	);
+
+	//terminal_set("window.cellsize=8x16");
 }
 
 int main()
